ca4488fb184415cfc9aed1ea439f667bf094a1ed
Myint <myint@ots.co.th>
2023-06-10 00:33:11 +0700

    fixed

--- app/Http/Controllers/User/UserIR/DividendpolicyandpaymentInformationController.php
+++ app/Http/Controllers/User/UserIR/DividendpolicyandpaymentInformationController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\User\UserIR;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DividendpolicyandpaymentInformationController extends Controller
 {
     public function index(){
-        return view('user.investorRealtions.investor-relations-dividend-information');
+
+        $type = DB::table('irbanners')->where('irtype', 'Dividendpolicy')->first();
+        $dividendDataLists = DB::table('dividenddatalists')->get();
+        $dividendpolicyandpayment = DB::table('dividendpolicyandpayments')->first();
+        return view('user.investorRealtions.investor-relations-dividend-information',compact('type', 'dividendDataLists', 'dividendpolicyandpayment'));
     }
 }
--- app/Http/Controllers/User/UserIR/FinancialInformationController.php
+++ app/Http/Controllers/User/UserIR/FinancialInformationController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\User\UserIR;
 
 use App\Http\Controllers\Controller;
+use App\Models\Type;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FinancialInformationController extends Controller
 {
     public function index()
     {
-        return view('user.investorRealtions.investor-relations-financial-info');
+
+
+        $type = DB::table('irbanners')->where('irtype', 'Dividendpolicy')->first();
+        dd($type);
+        $dividendDataLists = DB::table('dividenddatalists')->get();
+        return view('user.investorRealtions.investor-relations-financial-info', compact('type', 'dividendDataLists'));
     }
 }
